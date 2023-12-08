979fd10be65683d19af79f8add6d401a1b1f3055
maifzs <mailafaizashafira@gmail.com>
2023-12-08 09:01:39 +0700

    services

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -2,5 +2,12 @@
 import Contacts from "../models/contacts.js";
 
 export const newContact = async (name, email, subject, message) => {
-  return await Contacts.create({ name, email, subject, message });
+  try {
+    await Contacts.create({ name, email, subject, message });
+    // Don't return any data or return only what's necessary
+    return null;
+  } catch (error) {
+    console.error("Error creating new contact:", error);
+    throw error;
+  }
 };
