71796e72d470dc0e6470ed9d2aa3884d86677bee
maifzs <mailafaizashafira@gmail.com>
2023-12-09 11:01:50 +0700

    update controller lagi

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -8,6 +8,20 @@ import { newContact } from "../services/contactsServices.js";
  */
 export const postnewContact = async (request, response) => {
   const { name, email, subject, message } = request.body;
+  
+  try {
+    await newContact(name, email, subject, message);
 
-  await newContact(name, email, subject, message);
+    // Send a success response
+    response
+      .status(200)
+      .json({ success: true, message: "Data submitted successfully" });
+  } catch (error) {
+    console.error("Error processing new contact:", error);
+
+    // Send an error response
+    response
+      .status(500)
+      .json({ success: false, message: "Error submitting data" });
+  }
 };
