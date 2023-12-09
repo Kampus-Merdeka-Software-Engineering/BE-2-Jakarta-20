85f45145463871069283cddfdfa534fced68877b
maifzs <mailafaizashafira@gmail.com>
2023-12-09 12:25:41 +0700

    update controller

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -9,24 +9,16 @@ import { newContact } from "../services/contactsServices.js";
 export const postnewContact = async (request, response) => {
   const { name, email, subject, message } = request.body;
 
+  await newContact(name, email, subject, message);
 
-  try {
-    await newContact(name, email, subject, message);
-
-    // Embed JavaScript to show pop-up alert in the HTML response
-    const htmlResponse = `
-      <script>
-        alert('Data submitted successfully');
-        window.location.href = 'https://maifzs.github.io/repotestweb/contacts.html'; // Replace with the actual URL
-      </script>
-    `;
-
-    // Send the HTML response with embedded script
-    response.send(htmlResponse);
-  } catch (error) {
-    console.error("Error processing new contact:", error);
-
-    // You can embed an error message or script here if needed
-    response.status(500).send("Error submitting data");
-  }
+  const responseData = `
+    <p>Data submitted successfully</p>
+    <button onclick="goToPreviousPage()">Go to Previous Page</button>
+    <script>
+        function goToPreviousPage() {
+            window.history.back();
+        }
+    </script>
+  `;
+  response.status(201).send(responseData);
 };
